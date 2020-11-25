4e7fdd890c0cf2b9ab51127e51035d1cc88baf9f
mike <mikethecodegeek@gmail.com>
2020-11-25 14:17:50 -0800

    make a new tweet, oh yeah!!

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,8 +1,57 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.json({ message: "test tweets index" });
-});
+const db = require('../db/models');
+const { Tweet } = db;
+const { check, validationResult} = require('express-validator');
 
+
+
+const asyncHandler = handler => (req,res,next) => handler(req,res,next).catch(next)
+
+function tweetNotFoundError(tweetId) {
+    const error = new Error();
+    error.title = 'Tweet not found';
+    error.status = 404;
+    return error;
+}
+
+const handleValidationErrors = (req, res, next) => {
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+        const errors = validationErrors.array().map((error) => error.msg);
+    
+        const err = Error("Bad request.");
+        err.errors = errors;
+        err.status = 400;
+        err.title = "Bad request.";
+        return next(err);
+      }
+      next();
+  };
+
+router.get("/", asyncHandler(async (req, res, next) => {
+    let tweets = await Tweet.findAll();
+    res.json({tweets});
+}));
+
+router.get("/:id",asyncHandler(async (req, res, next) => {
+    let tweet = await Tweet.findByPk(req.params.id);
+    if (tweet) {
+        res.json({tweet});
+    } else {
+        next(tweetNotFoundError(req.params.id))
+    }
+}));
+
+
+router.post('/',[
+    check('message').exists(),
+    check('message').isLength({ max: 270 })
+  ],handleValidationErrors, asyncHandler(async (req,res,next) => {
+    const body = req.body;
+    let tweet = await Tweet.create({message:body.message})
+    res.json({tweet})
+
+}))
 module.exports = router;
